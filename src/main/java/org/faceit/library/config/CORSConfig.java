ce73e5cbe95a5b7edb298c785e3d356404c3e41c
VladyslavLn <vladyslav.liakhin@gmail.com>
2024-04-01 20:00:24 +0300

    Add CORS configuration

---
+++ src/main/java/org/faceit/library/config/CORSConfig.java
@@ -0,0 +1,24 @@
+package org.faceit.library.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CORSConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/v1/**")
+                        .allowedOrigins(
+                                "http://localhost:4200"
+                        )
+                        .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
+            }
+        };
+    }
+}
+
